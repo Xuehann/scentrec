a5308dee870e82c06db9c2dff8849c04f62cf473
Xinyi Liu <xliu0237@student.monash.edu>
2025-06-02 19:25:39 +0800

    add hyperparameters

--- hyperparameters.py
+++ hyperparameters.py
@@ -8,15 +8,15 @@ tool = ToolGeneral()
 class Hyperparams:
     '''Hyper parameters'''
     # Load sentiment dictionary
-    deny_word = tool.load_dict(os.path.join(pwd,'scraper','not.txt'))
-    posdict = tool.load_dict(os.path.join(pwd,'scraper','positive.txt'))
-    negdict = tool.load_dict(os.path.join(pwd,'scraper','negative.txt'))
+    deny_word = tool.load_dict(os.path.join(pwd,'data','not.txt'))
+    posdict = tool.load_dict(os.path.join(pwd,'data','positive.txt'))
+    negdict = tool.load_dict(os.path.join(pwd,'data','negative.txt'))
     pos_neg_dict = posdict|negdict
     # Load adverb dictionary
-    mostdict = tool.load_dict(os.path.join(pwd,'scraper','most.txt'))
-    verydict = tool.load_dict(os.path.join(pwd,'scraper','very.txt'))
-    moredict = tool.load_dict(os.path.join(pwd,'scraper','more.txt'))
-    ishdict = tool.load_dict(os.path.join(pwd,'scraper','ish.txt'))
-    insufficientlydict = tool.load_dict(os.path.join(pwd,'scraper','insufficiently.txt'))
-    overdict = tool.load_dict(os.path.join(pwd,'scraper','over.txt'))
-    inversedict = tool.load_dict(os.path.join(pwd,'scraper','inverse.txt'))
+    mostdict = tool.load_dict(os.path.join(pwd,'data','most.txt'))
+    verydict = tool.load_dict(os.path.join(pwd,'data','very.txt'))
+    moredict = tool.load_dict(os.path.join(pwd,'data','more.txt'))
+    ishdict = tool.load_dict(os.path.join(pwd,'data','ish.txt'))
+    insufficientlydict = tool.load_dict(os.path.join(pwd,'data','insufficiently.txt'))
+    overdict = tool.load_dict(os.path.join(pwd,'data','over.txt'))
+    inversedict = tool.load_dict(os.path.join(pwd,'data','inverse.txt'))
